cf557c09ad15f0eb07904b6cb96c198fd668f91a
muhsaputra <muhsaputrabiz@gmail.com>
2024-11-23 18:24:39 +0700

    Initial Commit

---
+++ Toko Asik Kremes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Toko Asik Kremes
+//
+//  Created by Muhamad Tri Saputra on 23/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
